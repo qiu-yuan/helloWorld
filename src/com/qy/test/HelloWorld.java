19a18f4ae5d5b68bd3141c1fede80d29697cf0ce
qiuyuan <1207589404@qq.com>
2016-07-18 10:42:09 +0800

    提交eclipse

--- src/com/qy/test/HelloWorld.java
+++ src/com/qy/test/HelloWorld.java
@@ -2,6 +2,6 @@ package com.qy.test;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("hello world 1111111111111111111111222222222222222");
+		System.out.println("hello world 11111111111111111111112222222222222224444444444444444444444");
 	}
 }
